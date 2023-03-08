ebe9a3ae1a4a2c1d87afb8b69762b333fadc1ba4
smiles30 <81188736+smiles30@users.noreply.github.com>
2023-03-07 22:39:03 -0500

    Update IScheduler.java

--- src/scheduler/IScheduler.java
+++ src/scheduler/IScheduler.java
@@ -1,10 +1,15 @@
 package scheduler;
 
-public interface IScheduler {
+import java.util.Queue;
+//import java.util.LinkedList;
 
+public interface IScheduler {
     //Defines the features the scheduler needs to support.
 
     //Load list of programs
+    public void loadProgram(Queue<Process> p);
     //Get next program to run
+    public Queue<Process> runNext();
     //Maintain state of programs
+
 }
