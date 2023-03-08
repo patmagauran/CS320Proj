25144f2e46fca4cbea36b7b3555ce31fc2d29960
AustinNash11 <103140505+AustinNash11@users.noreply.github.com>
2023-03-08 13:23:39 -0500

    Update Loader.java
    Loader class

--- src/loading/Loader.java
+++ src/loading/Loader.java
@@ -1,5 +1,35 @@
 package loading;
 
 public class Loader {
-    //Should load in Task list
+    static void Loadermethod(String[] args) 
+	        throws IOException
+	    {
+	        
+	        List<String> listOfStrings
+	            = new ArrayList<String>();
+	       
+	        
+	        BufferedReader bf = new BufferedReader(
+	            new FileReader("textFile.txt"));
+	       
+	        // read entire line as string
+	        String line = bf.readLine();
+	       
+	        // checking for end of file
+	        while (line != null) {
+	            listOfStrings.add(line);
+	            line = bf.readLine();
+	        }
+	        bf.close();
+	        
+	        // storing the data in arraylist to array
+	        String[] array
+	            = listOfStrings.toArray(new String[0]);
+	       
+	        // printing each line of file
+	        // which is stored in array
+	        for (String str : array) {
+	            System.out.println(str);
+	        }
+	    }//Should load in Task list
 }
